b9897780e957b9101a9e78a14839b1f09b8b2b26
SirRandoo <8146689+SirRandoo@users.noreply.github.com.>
2024-04-12 13:58:53 -0500

    Fixed mod settings being "read-only."
    Added missing "ref" keywords to appropriate UI methods, also cleaned up some of the artifacts that were produced by the decompilation process.

--- TwitchToolkit/TwitchToolkit.Settings/Settings_Coins.cs
+++ TwitchToolkit/TwitchToolkit.Settings/Settings_Coins.cs
@@ -8,16 +8,16 @@ public static class Settings_Coins
 {
 	public static void DoWindowContents(Rect rect, Listing_Standard optionsListing)
 	{
-        optionsListing.AddLabeledNumericalTextField((TaggedString)(Translator.Translate("TwitchToolkitStartingBalance")),  ToolkitSettings.StartingBalance, 0.8f);
-		optionsListing.SliderLabeled((TaggedString)(Translator.Translate("TwitchToolkitCoinInterval")),  ToolkitSettings.CoinInterval, Math.Round((float)ToolkitSettings.CoinInterval).ToString(), 1f, 15f);
-		optionsListing.SliderLabeled((TaggedString)(Translator.Translate("TwitchToolkitCoinAmount")),  ToolkitSettings.CoinAmount, Math.Round((float)ToolkitSettings.CoinAmount).ToString());
-		optionsListing.AddLabeledNumericalTextField((TaggedString)(Translator.Translate("TwitchToolkitMinimumPurchasePrice")),  ToolkitSettings.MinimumPurchasePrice, 0.8f);
-		((Listing)optionsListing).Gap(12f);
-		optionsListing.CheckboxLabeled((TaggedString)(Translator.Translate("TwitchToolkitUnlimitedCoins")), ref ToolkitSettings.UnlimitedCoins, (string)null);
-		((Listing)optionsListing).GapLine(12f);
-		((Listing)optionsListing).Gap(12f);
-		optionsListing.CheckboxLabeled((TaggedString)(Translator.Translate("TwitchToolkitChatReqsForCoins")), ref ToolkitSettings.ChatReqsForCoins, (string)null);
-		optionsListing.SliderLabeled((TaggedString)(Translator.Translate("TwitchToolkitTimeBeforeHalfCoins")),  ToolkitSettings.TimeBeforeHalfCoins, Math.Round((double)ToolkitSettings.TimeBeforeHalfCoins).ToString(), 15f, 120f);
-		optionsListing.SliderLabeled((TaggedString)(Translator.Translate("TwitchToolkitTimeBeforeNoCoins")),  ToolkitSettings.TimeBeforeNoCoins, Math.Round((double)ToolkitSettings.TimeBeforeNoCoins).ToString(), 30f, 240f);
+        optionsListing.AddLabeledNumericalTextField("TwitchToolkitStartingBalance".Translate(), ref ToolkitSettings.StartingBalance, 0.8f);
+		optionsListing.SliderLabeled("TwitchToolkitCoinInterval".Translate(),  ref ToolkitSettings.CoinInterval, Math.Round((float)ToolkitSettings.CoinInterval).ToString(), 1f, 15f);
+		optionsListing.SliderLabeled("TwitchToolkitCoinAmount".Translate(), ref ToolkitSettings.CoinAmount, Math.Round((float)ToolkitSettings.CoinAmount).ToString());
+		optionsListing.AddLabeledNumericalTextField("TwitchToolkitMinimumPurchasePrice".Translate(), ref ToolkitSettings.MinimumPurchasePrice, 0.8f);
+		optionsListing.Gap(12f);
+		optionsListing.CheckboxLabeled("TwitchToolkitUnlimitedCoins".Translate(), ref ToolkitSettings.UnlimitedCoins, null);
+		optionsListing.GapLine(12f);
+		optionsListing.Gap(12f);
+		optionsListing.CheckboxLabeled("TwitchToolkitChatReqsForCoins".Translate(), ref ToolkitSettings.ChatReqsForCoins, null);
+		optionsListing.SliderLabeled("TwitchToolkitTimeBeforeHalfCoins".Translate(),  ref ToolkitSettings.TimeBeforeHalfCoins, Math.Round((double)ToolkitSettings.TimeBeforeHalfCoins).ToString(), 15f, 120f);
+		optionsListing.SliderLabeled("TwitchToolkitTimeBeforeNoCoins".Translate(),  ref ToolkitSettings.TimeBeforeNoCoins, Math.Round((double)ToolkitSettings.TimeBeforeNoCoins).ToString(), 30f, 240f);
 	}
 }
--- TwitchToolkit/TwitchToolkit.Settings/Settings_Cooldowns.cs
+++ TwitchToolkit/TwitchToolkit.Settings/Settings_Cooldowns.cs
@@ -14,15 +14,15 @@ public static class Settings_Cooldowns
 		//IL_00be: Unknown result type (might be due to invalid IL or missing erences)
 		//IL_00e8: Unknown result type (might be due to invalid IL or missing erences)
 		//IL_011e: Unknown result type (might be due to invalid IL or missing erences)
-		optionsListing.SliderLabeled("Days per cooldown period",  ToolkitSettings.EventCooldownInterval, Math.Round((double)ToolkitSettings.EventCooldownInterval).ToString(), 1f, 15f);
-		((Listing)optionsListing).Gap(12f);
-		optionsListing.CheckboxLabeled((TaggedString)(Translator.Translate("TwitchToolkitMaxEventsLimit")), ref ToolkitSettings.MaxEvents, (string)null);
-		((Listing)optionsListing).Gap(12f);
-		optionsListing.AddLabeledNumericalTextField((TaggedString)(Translator.Translate("TwitchToolkitMaxBadEvents")),  ToolkitSettings.MaxBadEventsPerInterval, 0.8f);
-		optionsListing.AddLabeledNumericalTextField((TaggedString)(Translator.Translate("TwitchToolkitMaxGoodEvents")),  ToolkitSettings.MaxGoodEventsPerInterval, 0.8f);
-		optionsListing.AddLabeledNumericalTextField((TaggedString)(Translator.Translate("TwitchToolkitMaxNeutralEvents")),  ToolkitSettings.MaxNeutralEventsPerInterval, 0.8f);
-		optionsListing.AddLabeledNumericalTextField((TaggedString)(Translator.Translate("TwitchToolkitMaxItemEvents")),  ToolkitSettings.MaxCarePackagesPerInterval, 0.8f);
-		((Listing)optionsListing).Gap(12f);
-		optionsListing.CheckboxLabeled((TaggedString)(Translator.Translate("TwitchToolkitEventsHaveCooldowns")), ref ToolkitSettings.EventsHaveCooldowns, (string)null);
+		optionsListing.SliderLabeled("Days per cooldown period",  ref ToolkitSettings.EventCooldownInterval, Math.Round((double)ToolkitSettings.EventCooldownInterval).ToString(), 1f, 15f);
+		optionsListing.Gap(12f);
+		optionsListing.CheckboxLabeled("TwitchToolkitMaxEventsLimit".Translate(), ref ToolkitSettings.MaxEvents, null);
+		optionsListing.Gap(12f);
+		optionsListing.AddLabeledNumericalTextField("TwitchToolkitMaxBadEvents".Translate(), ref ToolkitSettings.MaxBadEventsPerInterval, 0.8f);
+		optionsListing.AddLabeledNumericalTextField("TwitchToolkitMaxGoodEvents".Translate(), ref ToolkitSettings.MaxGoodEventsPerInterval, 0.8f);
+		optionsListing.AddLabeledNumericalTextField("TwitchToolkitMaxNeutralEvents".Translate(), ref ToolkitSettings.MaxNeutralEventsPerInterval, 0.8f);
+		optionsListing.AddLabeledNumericalTextField("TwitchToolkitMaxItemEvents".Translate(), ref ToolkitSettings.MaxCarePackagesPerInterval, 0.8f);
+		optionsListing.Gap(12f);
+		optionsListing.CheckboxLabeled("TwitchToolkitEventsHaveCooldowns".Translate(), ref ToolkitSettings.EventsHaveCooldowns, null);
 	}
 }
